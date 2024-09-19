e128ab2194f394a4c5568ef9eb3f534ca46194e2
echadbourne <elizabeth.chadbourne@mymail.champlain.edu>
2024-09-19 14:14:05 -0400

    First changes to vconnect

--- automation/vconnect.py
+++ automation/vconnect.py
@@ -1,1 +1,12 @@
 #vconnect script
+
+import getpass
+passw = getpass.getpass()
+from pyVim.connect import SmartConnect
+import ssl
+s=ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+s.verify_mode=ssl.CERT_NONE
+si= SmartConnect(host="vcenter.elizabeth.local", user="elizabeth-adm@elizabeth.local", pwd=passw, sslcontext=s)
+aboutInfo=si.content.about
+print(aboutInfo)
+print(aboutInfo.fullName)
