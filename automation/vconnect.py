fa494ba097908bcdfb2a36a6498d0610e70e3fc1
echadbourne <elizabeth.chadbourne@mymail.champlain.edu>
2024-09-20 10:31:46 -0400

    progress on vmutils, import vconnect to vmutils

--- automation/vconnect.py
+++ automation/vconnect.py
@@ -1,12 +1,17 @@
 #vconnect script
 
 import getpass
-passw = getpass.getpass()
 from pyVim.connect import SmartConnect
 import ssl
-s=ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-s.verify_mode=ssl.CERT_NONE
-si= SmartConnect(host="vcenter.elizabeth.local", user="elizabeth-adm@elizabeth.local", pwd=passw, sslcontext=s)
-aboutInfo=si.content.about
-print(aboutInfo)
-print(aboutInfo.fullName)
+passw = getpass.getpass()
+
+
+def vconnectfunction():
+    s=ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    s.verify_mode=ssl.CERT_NONE
+    si= SmartConnect(host="vcenter.elizabeth.local", user="elizabeth-adm@elizabeth.local", pwd=passw, sslContext=s)
+    aboutInfo=si.content.about
+    print(aboutInfo)
+    print(aboutInfo.fullName)
+
+vconnectfunction()
--- automation/vm-utils.py
+++ automation/vm-utils.py
@@ -1,5 +1,5 @@
 # location for vm-utils script
-
+import vconnect
 #basic menu
 def menu():
     print("[1] VCenter Info")
@@ -8,17 +8,32 @@ def menu():
     print("[0] Exit the program")
 
 def vcenterinfo():
-    print("Display vcenter info")
+    #print("Display vcenter info")
+    print()
+    print("VCenter Info Selected")
+    print()
+    vconnect.vconnectfunction()
 
 def sessioninfo():
+    print()
+    print("Session Details Selected")
+    # Contains vcenter username, IP of mgmt box, and the domain name of the vcenter host
+    print("~~~~~~~~~~~~~~~~~~~~~~")
     print("Session Information")
+    print("~~~~~~~~~~~~~~~~~~~~~~")
 
 def vmdetails():
-    print("VM details")
+    print("VM Details Selected")
+    print("~~~~~~~~~~~~~~~~~~~~~~")
+    #print a list of all of the vms managed by vcenter
+    print("~~~~~~~~~~~~~~~~~~~~~~")
+    vm = str(input("Enter the name of your VM. Leave blank if you want all printed: "))
+    #match vm name to values, print metadata containing name, state, number of processors, 
+    #total memory (might need some math and logic) and ip address (install vmtools on pfsense)
 
 menu()
 print()
-option = int(input("Enter yout selection: "))
+option = int(input("Enter your selection: "))
 
 while option != 0:
     if option == 1:
