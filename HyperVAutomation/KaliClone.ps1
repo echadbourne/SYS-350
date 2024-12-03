b52d2da745ca048ec4c85f5209d2d52a4cc67c72
echadbourne <elizabeth.chadbourne@mymail.champlain.edu>
2024-12-03 12:09:33 -0500

    Ubuntu script done, kali script kinda done

---
+++ HyperVAutomation/KaliClone.ps1
@@ -0,0 +1,43 @@
+﻿#Automate the creation of a linked clone of kali
+Write-Host "This script will create a linked clone of the kali vm"
+
+#Shut down and snapshot the vm
+Write-Host "Stopping and snapshotting the target vm"
+Stop-VM -Name Kali
+#Start-Sleep -Seconds 5
+Write-Host "VM-Stopped"
+
+Get-VM -Name Kali | Checkpoint-VM -SnapshotName "Pre-parent Snapshot"
+Write-Host "Snapshot Created, Beginning clone operations"
+
+#Set the parent VHD to read only
+
+Set-ItemProperty -Path "C:\Users\Public\Documents\Hyper-V\Virtual Hard Disks\Kali.vhdx" `
+-Name IsReadOnly -Value $true
+
+Write-Host "Parent vhd set to read only"
+
+#Create a new virtual hard disk that points to the parent's VHD
+
+New-VHD -Path "C:\Users\Public\Documents\Hyper-V\Virtual Hard Disks\SonofKali.vhdx" `
+-ParentPath "C:\Users\Public\Documents\Hyper-V\Virtual Hard Disks\Kali.vhdx" -Differencing
+
+Write-Host "New VHD created"
+
+#Then create a new VM
+New-VM -Name "SonofKali" -MemoryStartupBytes 4GB -SwitchName "Hyper-V-Wan" -BootDevice VHD `
+-Path "C:\Users\Public\Documents\Hyper-V\Virtual Hard Disks\SonofKali" -Generation 2 
+
+Add-VMHardDiskDrive -VMName SonofKali -Path "C:\Users\Public\Documents\Hyper-V\Virtual Hard Disks\SonofKali.vhdx"
+#-NewVHDPath "C:\Users\Public\Documents\Hyper-V\Virtual Hard Disks\SonofKali.vhdx"`
+#-NewVHDSizeBytes 127GB
+
+Write-Host "Linked Clone Created!"
+
+$vhd = Get-VMHardDiskDrive -VMName SonofKali
+
+Set-VMFirmware -VMName SonofKali -BootOrder $vhd -EnableSecureBoot Off
+
+Write-Host "boot device set, secure boot off"
+
+Get-VMFirmware SonofKali | Select-Object *
---
+++ HyperVAutomation/sonofubuntuato.ps1
@@ -0,0 +1,50 @@
+﻿
+$Prompt = "Please choose your operation:`n"
+$Prompt += "0 - Exit`n"
+$Prompt += "1 - Start sonofubuntu`n"
+$Prompt += "2 - Stop sonofubuntu`n"
+$Prompt += "3 - Take a checkpoint of sonofubuntu`n"
+$Prompt += "4 - Switch sonofubuntu to another network`n"
+
+$operation = $true
+
+while ($operation){
+        Write-Host $Prompt | Out-String
+        $choice = Read-Host
+        if($choice -eq 0){
+            Write-Host "Goodbye" | Out-string
+            $operation = $false
+        }
+        elseif($choice -eq 1){
+            #Start vm
+            Write-Host "Starting sonofubuntu"
+            Start-VM -Name sonofubuntu
+            Write-Host "Done"
+        }
+        elseif($choice -eq 2){
+            #stop vm
+            Write-Host "Stopping sonofubuntu"
+            Stop-VM -Name sonofubuntu
+            Write-Host "Done"
+        }
+        elseif($choice -eq 3){
+            #Create a checkpoint
+
+            Write-Host "Please enter a name for your checkpoint"
+            $name = Read-Host
+            Get-VM -Name sonofubuntu | Checkpoint-VM -SnapshotName $name
+            Write-Host "Snapshot Created"
+        }
+        elseif($choice -eq 4){
+            #change vm network
+            Write-Host "Current vm network"
+            $network = Get-VM -Name sonofubuntu | Get-VMNetworkAdapter
+            Write-Host $network
+            Write-Host "Please enter the network you want to change sonofubuntu to"
+            $switch = Read-Host
+            Get-VM -Name sonofubuntu | Get-VMNetworkAdapter | Connect-VMNetworkAdapter -SwitchName $switch
+        }
+        else{
+            Write-Host "Please enter a valid option"
+        }
+}
